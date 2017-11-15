ae759e0b9337ae98b6271c20d162d281dc4d40b2
Xindi Xu <xindixu@utexas.edu>
2017-11-14 19:49:57 -0600

    Revert "reject the user to join a team after the team is full (5 people)"
    This reverts commit 59a42401c2cc97c660603d74fd4ef8bf15e32065.

--- Group15Alpha/JoinTeamViewController.swift
+++ Group15Alpha/JoinTeamViewController.swift
@@ -13,35 +13,19 @@ class JoinTeamViewController: UIViewController {
     
     @IBOutlet weak var codeTF: UITextField!
     var ref:DatabaseReference?
-    let maxNumOfPlayer = 5
     
     @IBAction func joinATeam(_ sender: Any) {
         let c = codeTF.text!.uppercased()
-        // no value entered
         if c == "" {
             createAlert(title: "Error", message: "Please enter a code")
         }
         else{
-            // get the data under "Games"
-            // this is node "Game"
-            _ = ref?.child("Games").observeSingleEvent(of:.value, with: {(snapshot) in
-                // code is valid
+            let handler = ref?.child("Games").observeSingleEvent(of:.value, with: {(snapshot) in
                 if snapshot.hasChild(c) {
                     print("valid code")
-                    let playerRef = self.ref?.child("Games/\(c)/players")
-                    // get the data uner "Games/<code>/players"
-                    _ = playerRef?.observeSingleEvent(of: .value, with: {(players) in
-                        // number of people in a team smaller than 5
-                        // reject request of joining a team 
-                        if players.childrenCount < self.maxNumOfPlayer {
-                            self.ref?.child("Games/\(c)/players").childByAutoId().setValue(Auth.auth().currentUser?.uid)
-                            let controaller = self.storyboard?.instantiateViewController(withIdentifier: "getReady")
-                            self.present(controaller!, animated: true, completion: nil)
-                        }
-                        else{
-                            self.createAlert(title: "Error", message: "The team is full")
-                        }
-                    })
+                    self.ref?.child("Games/\(c)/players").childByAutoId().setValue(Auth.auth().currentUser?.uid)
+                    let controaller = self.storyboard?.instantiateViewController(withIdentifier: "getReady")
+                    self.present(controaller!, animated: true, completion: nil)
                 }
                 else{
                     print("invalid code")
