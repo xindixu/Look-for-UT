382e23319c552ba2f23794422202f238cb414d91
Xindi Xu <xindixu@utexas.edu>
2017-11-14 19:44:08 -0600

    minor changes

---
+++ Group15Alpha/GamePrepViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GamePrepViewController.swift
+//  Group15Alpha
+//
+//  Created by Xindi Xu on 11/14/17.
+//  Copyright © 2017 Group15. All rights reserved.
+//
+
+import UIKit
+
+class GamePrepViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
